db03ec12779d4feb08b39fbaeba18d49aaf40d03
πe0 <109039431+franfran23@users.noreply.github.com>
2024-02-22 17:58:14 +0100

    Tests chat
    ajout websocket (chat en direct uniquement, aucune sauvegarde des msg)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, url_for, request, redirect
-import mysql.connector
+from flask import Flask, url_for, request, redirect, render_template
+from flask_socketio import SocketIO, emit
+# import mysql.connector
 
+'''
 def connect_db(host='localhost', user='root', password='', db=None):
 	return mysql.connector.connect(
 		host=host,
@@ -9,11 +11,31 @@ def connect_db(host='localhost', user='root', password='', db=None):
 		database=db)
 
 db = connect_db(user='testuser', password='testuser', db='suivi_medical')
-cursor = db.cursor()
+cursor = db.cursor()'''
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
-
+# FLASK SERVER
 @app.route('/')
 def index():
-	return open('index.html', 'r', encoding='utf8').read()
+	return open('index.html', 'r', encoding='utf').read()
+
+# SOCKETIO SERVER
+@socketio.on('connect')
+def handle_connect():
+	print('Connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+	print('Disconnected')
+
+@socketio.on('message')
+def handle_message(message):
+	print('Received message:', message)
+	emit('message', message, broadcast=True)
+
+
+if __name__ == '__main__':
+	socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+	
