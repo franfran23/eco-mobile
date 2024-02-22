e9c8f7a31cc20b40d659d2ca8798dc26c7b7bbca
πe0 <109039431+franfran23@users.noreply.github.com>
2024-02-22 16:36:43 +0100

    add index page
    empty page

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 from flask import Flask, url_for, request, redirect
+import mysql.connector
+
+def connect_db(host='localhost', user='root', password='', db=None):
+	return mysql.connector.connect(
+		host=host,
+		user=user,
+		password=password,
+		database=db)
+
+db = connect_db(user='testuser', password='testuser', db='suivi_medical')
+cursor = db.cursor()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return open('index.html', 'r', encoding='utf8').read()
